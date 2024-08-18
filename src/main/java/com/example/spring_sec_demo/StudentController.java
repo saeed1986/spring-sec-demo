b6949c7f4089424b82d388c6ac1c016efc77590d
saeed rastegar <saeed.rastegarrazaghi@tu-clausthal.de>
2024-08-18 14:11:23 +0200

    initial commit

---
+++ src/main/java/com/example/spring_sec_demo/StudentController.java
@@ -0,0 +1,34 @@
+package com.example.spring_sec_demo;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+    List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "saeed", "java"),
+            new Student(2, "behdis", "ui")
+    ));
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @PostMapping("students")
+    public void addStudent(@RequestBody Student student){
+        students.add(student);
+    }
+}
